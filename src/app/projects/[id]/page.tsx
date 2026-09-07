858f4ed0f6c4faf550cbe474910c58e7765ec463
Hazel Hyeseon Choi <148557614+Hazel-Choi@users.noreply.github.com>
2026-09-07 13:40:03 +0100

    Pass projectCode prop to ProjectTimeline component

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -77,6 +77,7 @@ export default async function ProjectDetailPage({
       </div>
 
       <ProjectTimeline
+        projectCode={id}
         milestones={project.milestones}
         sprints={project.sprints}
         holidays={project.holidays}
